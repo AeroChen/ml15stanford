01f4526e43abd94f7b5705cd5047cd48e7d831c4
Xichen Shi <sxc901231@gmail.com>
2015-02-18 23:08:02 -0600

    Use vectorized implementation on gradient descent

--- mlclass-ex1/gradientDescentMulti.m
+++ mlclass-ex1/gradientDescentMulti.m
@@ -16,10 +16,7 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
-    t = theta;
-    for i = 1:size(X,2);
-        theta(i) = t(i) - alpha/m * sum((X*t - y).*X(:,i));
-    end;
+    theta = theta - alpha/m * X' * (X*theta - y);
     
     
     % ============================================================
